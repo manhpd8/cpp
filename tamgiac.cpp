7b3fde53cbed86e016a9c680b75308bb32a13212
manhpd8 <manhpd.work@gmail.com>
2022-07-06 20:06:38 +0700

    fix tam giac

--- tamgiac.cpp
+++ tamgiac.cpp
@@ -4,15 +4,13 @@ main()
 {
 	int n;
 	cin>>n;
-	int hang =1;
 	for(int i=1;i<=n;i++) {
 		for(int j=1;j<=n-i;j++) {
 			cout<<" ";
 		}
-		for(int k=1;k<=hang;k++){
+		for(int k=1;k<=i;k++){
 			cout<<"* ";
 		}
 		cout<<endl;
-		hang++;
 	}
 }
